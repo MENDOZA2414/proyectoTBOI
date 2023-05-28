bee58499f2ae814cc3cbf122f5553399ed55bfd2
Mario Alberto <Mario Alberto@LAPTOP-J74QFDR5>
2023-05-27 21:23:21 -0600

    Clase objetos

---
+++ ProyectoTBOI/src/Objeto.java
@@ -0,0 +1,34 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Objeto {
+	private int tamaño;
+    private BufferedImage sprite;
+    private int x;
+    private int y;
+
+    public Objeto(String spritePath, int tamaño, int x, int y) {
+    	this.tamaño = tamaño;
+        try {
+            this.sprite = ImageIO.read(new File(spritePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public BufferedImage getSprite() {
+		return sprite;
+	}
+}
